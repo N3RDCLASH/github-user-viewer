96079a28755ffffed689cff50c8ba25d525ebdb8
Joel Naarendorp <joelhammen11@gmail.com>
2020-12-30 09:56:23 -0300

    added user profile layout

---
+++ src/views/User/User.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const User = () => {
+    
+    return (
+        <div className='row'>
+            <div className="col m4" id="profile">
+                <div className="col m8 offset-m2">
+                    <img src="" alt=""/>
+                </div>
+            </div>
+            <div className="col m4" id="projects"></div>
+            <div className="col m4" id="followers"></div>
+        </div>
+    )
+}
+
+export default User
