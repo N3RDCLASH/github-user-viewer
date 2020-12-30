f38c0bcc2ff988090bdbe53aaa975396a1422bae
Joel Naarendorp <joelhammen11@gmail.com>
2020-12-30 09:54:53 -0300

    added not found page

---
+++ src/views/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../views/NotFound/NotFound.css'
+const NotFound = () => {
+    return (
+        <>
+            <div id="notfound_graphic">
+            </div>
+            <h5 className="center">The page your looking for couldn't be found!</h5>
+        </>
+    )
+}
+
+export default NotFound
