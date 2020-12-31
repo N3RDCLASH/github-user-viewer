00d5ad9cf1a63693e96eef451ec29d424b573949
Joel Naarendorp <joelhammen11@gmail.com>
2020-12-31 14:22:06 -0300

    added github pages 404 redirect

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import Home from '../src/views/Home/Home'
 import User from '../src/views/User/User';
 import Search from '../src/views/Search/Search'
 import Footer from './layout/Footer';
-import { BrowserRouter as Router,Switch,Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import NotFound from './views/NotFound/NotFound';
 function App() {
   return (
     <>
-      <Router>
-         <TopNav/>
-         <div id="main">
-               <Switch>
-                    <Route path='/' exact component={Home}></Route>
-                    <Route path='/search/:query' component={Search} ></Route>
-                    <Route path='/user/:username' component={User} ></Route>
-                    <Route path='*' component={NotFound}></Route>
-                </Switch>
-         </div>
-         <Footer/>
+      <Router basename={process.env.PUBLIC_URL}>
+        <TopNav />
+        <div id="main">
+          <Switch>
+            <Route path='/' exact component={Home}></Route>
+            <Route path='/search/:query' component={Search} ></Route>
+            <Route path='/user/:username' component={User} ></Route>
+            <Route path='*' component={NotFound}></Route>
+          </Switch>
+        </div>
+        <Footer />
       </Router>
 
     </>
